c4a34c0b87915a0e8c7a99309d8fbd0b75e002fe
Arkadiy Korotaev <koriaf@mooserv.org>
2015-08-06 15:11:22 +0400

    nice base template

--- src/abudget/settings/base.py
+++ src/abudget/settings/base.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'abudget',
     'abudget.money',
 )
 
--- src/abudget/urls.py
+++ src/abudget/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
 ]
