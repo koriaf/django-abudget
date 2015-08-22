d6c4d37f2e4d776d268646eb8e76e3a2ad0bca8c
Arkadiy Korotaev <koriaf@mooserv.org>
2015-08-22 23:34:02 +0400

    go travis 3

--- test-dev.sh
+++ test-dev.sh
@@ -3,4 +3,4 @@ set -e
 ROOT=`dirname "${BASH_SOURCE[0]}"`
 export PYTHONDONTWRITEBYTECODE='dontwrite'
 export DJANGO_SETTINGS_MODULE=abudget.settings.test
-${ROOT}/venv.sh ./src/manage.py check
+./src/manage.py check
