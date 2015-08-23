59fca76300c0d91948fa8b58b2c6d21ddb6908a2
Arkadiy Korotaev <koriaf@mooserv.org>
2015-08-23 14:06:05 +0400

    money income form fix and some deployment-related config fixes

--- src/abudget/wsgi.py
+++ src/abudget/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for abudget project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abudget.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abudget.settings")
-
 application = get_wsgi_application()
